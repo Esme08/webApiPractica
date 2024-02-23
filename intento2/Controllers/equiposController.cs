10d947303d2292fdd9fe47e40cfdf7bd702ee040
esmer <esmer@LAPTOP-2CJ02D72>
2024-02-23 12:38:38 -0600

    Agregar archivos de proyecto.

---
+++ intento2/Controllers/equiposController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using intento2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace intento2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContext;
+        public equiposController(equiposContext equiposContext)
+        {
+            _equiposContext = equiposContext;
+        }
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            try
+            {
+
+                List<Equipos> listadoEquipos = (from e in _equiposContext.Equipos
+                                                select e).ToList();
+                if (listadoEquipos.Count == 0)
+                {
+                    return NotFound();
+                }
+                return Ok(listadoEquipos);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ intento2/Models/equiposContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace intento2.Models
+{
+    public class equiposContext : DbContext
+    {
+        public equiposContext(DbContextOptions<equiposContext> options) : base(options)
+        { }
+
+        public DbSet<Equipos> Equipos { get; set; }
+    }
+}
